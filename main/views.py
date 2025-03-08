a765e751a964e48f4fed4b631c4fbae359fb8dea
ottobrew <26239254+ottobrew@users.noreply.github.com>
2025-03-08 15:48:33 -0600

    Refresh UI; add 5 day forecast

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from datetime import datetime
 from .models import Parks
 from django.views.generic import TemplateView
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView
 from django.conf import settings
 from datetime import datetime 
 
@@ -29,18 +29,30 @@ def parks_detail(request, pk):
 
     try:
         park = Parks.objects.get(pk=pk)
+
+        # Get Current Weather Data
         r = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=' + str(park.latitude) + '&lon=' + str(park.longitude) + '&appid=' + settings.API_KEY + '&units=imperial')
         data = r.json()
+
+        # Get 5 Day Forecast Data
+        f = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(park.latitude) + '&lon=' + str(park.longitude) + '&appid=' + settings.API_KEY + '&units=imperial')
+        fdata = f.json()
 
         # Access Python dictionary and convert to CDT
         sunrise = data["sys"]["sunrise"] + data["timezone"]
         sunset = data["sys"]["sunset"] + data["timezone"]
 
         # Format datetime
-        m = datetime.fromtimestamp(sunrise).strftime('%H:%M:%S %D')
-        n = datetime.fromtimestamp(sunset).strftime('%H:%M:%S %D')
+        m = datetime.fromtimestamp(sunrise).strftime('%I:%M:%S %p %D')
+        n = datetime.fromtimestamp(sunset).strftime('%I:%M:%S %p %D')
+
+        # Format forecast times
+        for forecast in fdata['list']:
+            forecast['local_time'] = datetime.fromtimestamp(forecast['dt'] + data['timezone']).strftime('%D %I:%M %p')
+
+        print(data)
 
     except Parks.DoesNotExist:
         raise Http404("Sorry, no park found here.")    
 
-    return render(request, "main/parks_detail.html", {'park':park, 'data':data, 'm':m, 'n':n})   
+    return render(request, "main/parks_detail.html", {'park':park, 'data':data, 'fdata':fdata, 'm':m, 'n':n })   
